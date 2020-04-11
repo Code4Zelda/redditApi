292d17d719a14751e4d4ea5bedd9131c74f75dda
Donté Nall <code4zelda@gmail.com>
2020-04-11 16:40:02 -0700

    react running

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import "./App.css";
+// import RedditPost from "./Components/RedditPosts";
+
+const App = () => {
+  return (
+    <>
+      Hello world
+      {/* <RedditPost /> */}
+    </>
+  );
+};
+
+export default App;
