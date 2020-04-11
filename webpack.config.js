c2ed5800aac90dbd047c7690c644e7960a9a148a
Donté Nall <code4zelda@gmail.com>
2020-04-11 16:31:22 -0700

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  target: "web",
+  devtool: "source-map",
+  mode: "development",
+  // watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: path.resolve(__dirname, "node_modules"),
+        include: path.resolve(__dirname, "src"),
+        use: "ts-loader",
+      },
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(jpg|png|svg)$/,
+        loader: "url-loader",
+        options: {
+          limit: 25000,
+        },
+      },
+      {
+        test: /\.(jpg|png|svg)$/,
+        loader: "file-loader",
+        options: {
+          name: "[path][name].[hash].[ext]",
+        },
+      },
+    ],
+  },
+  resolve: { extensions: ["*", ".js", ".ts", ".json", ".tsx", "png"] },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "bundle.js",
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public/"),
+    port: 3001,
+    publicPath: "http://localhost:3001/dist/",
+    hot: true,
+    historyApiFallback: true,
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+  ],
+};
